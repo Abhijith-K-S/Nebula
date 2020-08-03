ed83839f31845c99e179719a29721fd87490bf71
Abhijith-K-S <abhijithks1729@gmail.com>
2020-08-03 19:58:07 +0530

    Create collision.cs

---
+++ Assets/collision.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class collision : MonoBehaviour
+
+{
+    public movement move;
+
+    void OnCollisionEnter(Collision info)
+    {
+        if(info.collider.tag == "obstacle")
+        {
+            move.enabled = false;
+ 
+        }
+        
+    }
+}
