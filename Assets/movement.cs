4b6845bd0ee4ab9224ae68c7036c92dad0b1f77a
Abhijith-K-S <abhijithks1729@gmail.com>
2020-08-03 19:58:21 +0530

    Create movement.cs

---
+++ Assets/movement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+
+
+public class movement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float Dforce = 200;
+    public float Sforce = 500;
+
+    void Start()
+    {
+
+
+    }
+
+
+   void FixedUpdate()
+   {
+        rb.useGravity = true;
+
+        //driving force of the ball
+        rb.AddForce(-Dforce * Time.deltaTime, 0,0);
+
+
+        //direction controls
+        if (Input.GetKey("d")||Input.GetKey("right"))
+        {
+            rb.AddForce(0, 0, Sforce * Time.deltaTime);   
+        }
+
+        if (Input.GetKey("a") || Input.GetKey("left"))
+        {
+            rb.AddForce(0, 0, -Sforce * Time.deltaTime);
+        }
+
+
+
+    }
+
+
+}
