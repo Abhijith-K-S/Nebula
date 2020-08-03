b303c613388f5bb3c4167e8c104de80ce17ff718
Abhijith-K-S <abhijithks1729@gmail.com>
2020-08-03 19:58:15 +0530

    Create follow.cs

---
+++ Assets/follow.cs
@@ -0,0 +1,18 @@
+﻿
+
+using UnityEngine;
+
+public class follow : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 gap;
+
+    
+    void Update()
+    {
+        transform.position = player.position + gap;
+        
+    }
+
+
+}
