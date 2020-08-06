39b387fc94b1a92d297d5651c3d31630e5cdd9f5
Abhijith-K-S <abhijithks1729@gmail.com>
2020-08-06 21:32:36 +0530

    startup countdown

--- Assets/scripts/movement.cs
+++ Assets/scripts/movement.cs
@@ -10,37 +10,42 @@ public class movement : MonoBehaviour
     public Rigidbody rb;
     public float Dforce = 50;
     public float Sforce = 50;
+    float time = 3.0f;
+
+    public GameObject intro;
 
 
     void Start()
     {
-
-
+        intro.SetActive(true);
+        
     }
 
 
    void FixedUpdate()
    {
         rb.useGravity = true;
-
-        //driving force of the ball
-        rb.AddForce(-Dforce * Time.deltaTime, 0,0);
-
-
-        //direction controls
-        if (Input.GetKey("d")||Input.GetKey("right"))
+        if (time >= 0)
         {
-            rb.AddForce(0, 0, Sforce * Time.deltaTime);   
+            time -= Time.deltaTime;
+            return;
         }
-
-        if (Input.GetKey("a") || Input.GetKey("left"))
+        else
         {
-            rb.AddForce(0, 0, -Sforce * Time.deltaTime);
+            //driving force of the ball
+            rb.AddForce(-Dforce * Time.deltaTime, 0, 0);
+
+
+            //direction controls
+            if (Input.GetKey("d") || Input.GetKey("right"))
+            {
+                rb.AddForce(0, 0, Sforce * Time.deltaTime);
+            }
+
+            if (Input.GetKey("a") || Input.GetKey("left"))
+            {
+                rb.AddForce(0, 0, -Sforce * Time.deltaTime);
+            }
         }
-
-
-
     }
-
-
 }
