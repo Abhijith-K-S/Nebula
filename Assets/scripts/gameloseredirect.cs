10e7605c87f4d3348bfad2b15320449a6ba87315
Abhijith-K-S <abhijithks1729@gmail.com>
2020-08-07 19:31:15 +0530

    redirecting functionality of gameover screen

---
+++ Assets/scripts/gameloseredirect.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class gameloseredirect : MonoBehaviour
+{
+    public GameObject button;
+
+    public void redirect()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+}
